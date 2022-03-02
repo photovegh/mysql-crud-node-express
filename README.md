56132677aeb1867ad36749ba6fd782845329755a
Tibor Végh <73848390+photovegh@users.noreply.github.com>
2022-03-02 17:49:28 +0100

    MySQL NodeJs Express CRUD

---
+++ README.md
@@ -0,0 +1,13 @@
+#Node.js, Express & MySQL Tutorial.
+
+// BUG:
+// BUG: szal a .env constansa hibásasan van definiálva
+// BUG: mivel a USERNAME-re a szg. valódi felhasználóját
+// BUG: hozza ki = veght a web_app helyett így azt át kell írni
+// BUG: INFO: **_ USER-re _**
+// BUG:
+
+In this video, we will use the Express framework with HTML, CSS, and JavaScript to build a simple CRUD (Create, Read, Update, Delete) App.
+I will show you how to make queries using MySQL and how to connect that to our express app to build a fullstack application.
+
+https://youtu.be/vrj9AohVhPA
---
+++ server/app.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const dbService = require("./dbService");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// NOTE: create  NOTE:
+app.post("/insert", (request, response) => {
+    const { name } = request.body;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.insertNewName(name);
+
+    result
+        .then((data) => response.json({ data: data }))
+        .catch((err) => console.log(err));
+});
+
+// NOTE: read  NOTE:
+app.get("/getAll", (request, response) => {
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.getAllData();
+
+    result
+        .then((data) => response.json({ data: data }))
+        .catch((err) => console.log(err));
+});
+
+// NOTE: update  NOTE:
+app.patch("/update", (request, response) => {
+    const { id, name } = request.body;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.updateNameById(id, name);
+
+    result
+        .then((data) => response.json({ success: data }))
+        .catch((err) => console.log(err));
+});
+
+// NOTE: delete  NOTE:
+app.delete("/delete/:id", (request, response) => {
+    const { id } = request.params;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.deleteRowById(id);
+
+    result
+        .then((data) => response.json({ success: data }))
+        .catch((err) => console.log(err));
+});
+
+// NOTE: search  NOTE:
+app.get("/search/:name", (request, response) => {
+    const { name } = request.params;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.searchByName(name);
+
+    result
+        .then((data) => response.json({ data: data }))
+        .catch((err) => console.log(err));
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("server is running ... (port) :" + process.env.PORT);
+});
